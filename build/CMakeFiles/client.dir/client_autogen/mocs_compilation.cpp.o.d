d114d2dc5e1322716e5e95877de6513873764947
Oliwia Tarent <100858979+oliwiatarent@users.noreply.github.com>
2025-12-13 20:54:45 +0100

    Delete build directory

--- build/CMakeFiles/client.dir/client_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/client.dir/client_autogen/mocs_compilation.cpp.o: \
-  /Users/oliwia/Documents/GitHub/panstwa-miasta-gra-sieciowa/build/client_autogen/mocs_compilation.cpp
--- build/CMakeFiles/server.dir/server_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/server.dir/server_autogen/mocs_compilation.cpp.o: \
-  /Users/oliwia/Documents/GitHub/panstwa-miasta-gra-sieciowa/build/server_autogen/mocs_compilation.cpp
