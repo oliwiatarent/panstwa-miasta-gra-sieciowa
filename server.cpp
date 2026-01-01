1f5396574547ede55d5e1928a6d6477bca288b2f
Oliwia Tarent <oliwia.tarent@student.put.poznan.pl>
2026-01-01 15:31:58 +0100

    Wybór dowolnego adresu IP dla serwera

--- server.cpp
+++ server.cpp
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <signal.h>
 #include <poll.h>
+#include <netdb.h>
+#include <ifaddrs.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -221,10 +223,52 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    struct ifaddrs *ifaddr;
+
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs failed");
+        return 1;
+    }
+
+    printf("Dostepne adresy IP:\n");
+    for (struct ifaddrs *ifa = ifaddr; ifa; ifa = ifa->ifa_next) {
+        if (!ifa->ifa_addr) continue;
+
+        if (ifa->ifa_addr->sa_family == AF_INET) {
+            sockaddr_in *addr = (sockaddr_in *)ifa->ifa_addr;
+            printf("%s\n", inet_ntoa(addr->sin_addr));
+        }
+    }
+
+    char ip[40];
+    int bytes = read(0, ip, 40);
+    if (bytes == -1) {
+        perror("Nadawanie adresu IP nie powiodlo sie");
+        return 1;
+    }
+    ip[bytes - 1] = '\0';
+
+    bool address_found = false;
+    for (struct ifaddrs *ifa = ifaddr; ifa; ifa = ifa->ifa_next) {
+        if (!ifa->ifa_addr) continue;
+
+        if (ifa->ifa_addr->sa_family == AF_INET) {
+            sockaddr_in *addr = (sockaddr_in *)ifa->ifa_addr;
+            if (strcmp(inet_ntoa(addr->sin_addr), ip) == 0) {
+                address_found = true;
+            }
+        }
+    }
+
+    if (!address_found) {
+        printf("Podano nie prawidlowy adres IP\n");
+        return 1;
+    }
+
     int servSock = socket(AF_INET, SOCK_STREAM, 0);
 
     sockaddr_in serverAddr = {};
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_addr.s_addr = inet_addr(ip);
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(atoi(argv[1]));
 
@@ -237,6 +281,8 @@ int main(int argc, char **argv)
         close(servSock);
         return 1;
     }
+
+    printf("Uruchomiono serwer na adresie %s:%s\n", ip, argv[1]);
 
     listen(servSock, SOMAXCONN);
 
